92326ff539c7de59bc6b844aa5c15e806805f4db
Chandrashekar <chandrashekar998@gmail.com>
2017-03-27 23:54:16 +0530

    Adding socket.io library and running server using http module

--- server/server.js
+++ server/server.js
@@ -1,13 +1,15 @@
 const path = require('path');
 var express = require('express');
-
+const socketIO= require('socket.io');
+const http=require('http');
 var app =express();
 
-const publicPath=path.join(__dirname+'/../public');
+const publicPath=path.join(__dirname+'./../public');
 console.log(publicPath);
 
+var server=http.createServer(app);
 app.use(express.static(publicPath));
 
-app.listen(3000,()=>{
+server.listen(3000,()=>{
   console.log('Server is up on port 3000');
 });
